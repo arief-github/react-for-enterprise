52f6c41c97dea3c7f1f4a4a1404d01f6ddabb465
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2025-04-02 23:18:04 +0700

    init: performing lazy-loading and code-splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { Suspense, lazy } from 'react';
 import './App.css';
-import BussinessCard from '@/components/BussinessCard/BussinessCard';
+
+import { Link, Route, Routes } from 'react-router-dom';
+import Spinner from '@/components/Spinner';
+
+const About = lazy(() => import('./views/About'));
+const Home = lazy(() => import('./views/Home'));
+const Contact = lazy(() => import('./views/Contact'));
 
 function App() {
   return (
     <div className='App mx-auto max-w-6xl text-center my-8'>
-      <h1 className='font-semibold text-2xl'>Bussiness Form</h1>
-      <BussinessCard />
+      <div className='my-8'>
+        <nav className='space-x-4'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </nav>
+      </div>
+
+      <Suspense
+        fallback={
+          <div className='flex justify-center'>
+            <Spinner show delay={500} />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
