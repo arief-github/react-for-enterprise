562659fc60b16a390211ae6dc8f48b1058521545
arief-github <ariefkurniawan0411@gmail.com>
2025-05-03 11:28:59 +0700

    derived state selector

--- src/components/EventsManager/components/DisplayEvents.tsx
+++ src/components/EventsManager/components/DisplayEvents.tsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
 import shallow from 'zustand/shallow';
 import { EventsState, useEventsStore } from '../eventsStore';
+import type { Event } from '../eventsTypes';
+import EventsTab, { EventTab } from './EventsTab';
+
+const pastAndUpcomingEventsSelector = (state: EventsState) => {
+  const upcomingEvents: Event[] = [];
+  const pastEvents: Event[] = [];
+  for (const event of state.events) {
+    const [day, month, year] = event.endDate
+      .split('/')
+      .map((item) => parseInt(item));
+    const [hour, minute] = event.endTime.split(':');
+    const isUpcoming =
+      new Date(year, month - 1, day, parseInt(hour), parseInt(minute)) >
+      new Date();
+    if (isUpcoming) {
+      upcomingEvents.push(event);
+    } else {
+      pastEvents.push(event);
+    }
+  }
+  return {
+    upcomingEvents,
+    pastEvents,
+  };
+};
 
 const DisplayEvents = () => {
+  const [eventsToShow, setEventsToShow] = useState<EventTab>('all');
   const { allEvents, selectEvent } = useEventsStore(
     (state: EventsState) => ({
       allEvents: state.events,
@@ -10,12 +37,26 @@ const DisplayEvents = () => {
     shallow
   );
 
+  const { pastEvents, upcomingEvents } = useEventsStore(
+    pastAndUpcomingEventsSelector,
+    shallow
+  );
+
+  const eventsMap: Record<EventTab, Event[]> = {
+    all: allEvents,
+    upcoming: upcomingEvents,
+    past: pastEvents,
+  };
+
+  const events = eventsMap[eventsToShow];
+
   return (
     <>
       <h2 className='font-semibold text-xl mb-4'>Events</h2>
+      <EventsTab activeTab={eventsToShow} setActiveTab={setEventsToShow} />
       <div className='mt-4'>
         <ul className='text-left shadow py-4 space-y-3 divide-y'>
-          {Array.isArray(allEvents) && allEvents.length ? (
+          {Array.isArray(events) && events.length ? (
             allEvents.map((event) => {
               return (
                 <li key={event.id} className='mt-3'>
---
+++ src/components/EventsManager/components/EventsTab.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+
+export type EventTab = 'all' | 'upcoming' | 'past';
+
+type EventTabProps = {
+  activeTab: EventTab;
+  setActiveTab: (tab: EventTab) => void;
+};
+
+const activeTabClass = '!border-blue-500 text-blue-500';
+const tabClass = 'px-2 pb-2 border-b border-transparent';
+
+const EventsTab = (props: EventTabProps) => {
+  const { activeTab, setActiveTab } = props;
+
+  return (
+    <div className='border-b border-blue-100 flex gap-4'>
+      <button
+        className={clsx(tabClass, activeTab === 'all' && activeTabClass)}
+        onClick={() => setActiveTab('all')}
+      >
+        All
+      </button>
+      <button
+        className={clsx(tabClass, activeTab === 'upcoming' && activeTabClass)}
+        onClick={() => setActiveTab('upcoming')}
+      >
+        Upcoming
+      </button>
+      <button
+        className={clsx(tabClass, activeTab === 'past' && activeTabClass)}
+        onClick={() => setActiveTab('past')}
+      >
+        Past
+      </button>
+    </div>
+  );
+};
+
+export default EventsTab;
